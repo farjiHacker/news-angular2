471d5ec721d19f90fb6245fdc06e1a1a5a640c22
Maulik Patel <km8295@gmail.com>
2018-01-11 17:46:09 +0530

    1st Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,36 +3,8 @@ import { Routes } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  //templateUrl: './app.component.html',
-  
-  styleUrls: ['./app.component.css'],
-  template: `
-  <div class="demo-layout-transparent mdl-layout mdl-js-layout">
-    <header class="mdl-layout__header mdl-layout__header--transparent">
-      <div class="mdl-layout__header-row">
-
-        <!-- Title -->
-        <span class="mdl-layout-title">Scotch Pets</span>
-
-        <!-- Add spacer, to align navigation to the right -->
-        <div class="mdl-layout-spacer"></div>
-
-        <!-- Navigation with router directives-->
-        <nav class="mdl-navigation">
-          <a class="mdl-navigation__link" [routerLink]="['/technology']">Cats</a>
-          <a class="mdl-navigation__link" [routerLink]="['/business']">Dogs</a>
-        </nav>
-      </div>
-    </header>
-
-    <main class="mdl-layout__content">
-      <h1 class="header-text">We care about pets...</h1>
-    </main>
-  </div>
-
-  <!-- Router Outlet -->
-  <!--<router-outlet></router-outlet>-->
-`
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   //constructor(router:Router) {}
