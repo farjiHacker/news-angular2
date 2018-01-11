ab01c6b49e41235e380357cf33810895ee3b019c
Maulik Patel <km8295@gmail.com>
2018-01-11 14:13:50 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
+import { ModuleWithProviders }  from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TechnologyComponent } from './technology/technology.component';
+import { BusinessComponent } from './business/business.component';
+
+export const routes: Routes = [
+  { path: 'technology', component: TechnologyComponent },
+  { path: 'business', component: BusinessComponent },
+];
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(routes);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TechnologyComponent,
+    BusinessComponent
   ],
   imports: [
     BrowserModule,
